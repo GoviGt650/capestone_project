6c8cc84557fcdc5f2128eb1f48fab14d8deae27c
Govind Jadapalli <jadapalli.govind@tudip.com>
2026-04-23 18:47:26 +0530

    All done and working and also tested by frontend

--- frontend/config.js
+++ frontend/config.js
@@ -1,9 +1,9 @@
 // Base URL → NGINX (Docker)
-window.BASE_URL = "http://localhost:8080";
+window.BASE_URL = "http://localhost:8081";
 
 // Choose backend here
 // options: "fastapi", "django", "node", "dotnet"
-window.SERVICE = "node";
+window.SERVICE = "dotnet";
 
 // Final API base (used in script.js)
 window.API = `${window.BASE_URL}/${window.SERVICE}`;
